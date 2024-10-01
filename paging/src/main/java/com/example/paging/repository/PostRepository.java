990d77612727f0d7b164a54bb7311ae11ec537d1
DESKTOP-NKPAB8C\noua0 <noua026@gmail.com>
2024-10-02 00:30:25 +0900

    삭제

--- paging/src/main/java/com/example/paging/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.paging.repository;
-
-import com.example.paging.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
